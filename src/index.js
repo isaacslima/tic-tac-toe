0286d280bd8bc1c4be20139ae80e5bf32ab01f4a
Isaac Sousa <isaaslima@yahoo.com.br>
2021-10-30 20:08:40 -0300

    add import game on index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Game from './game'
 
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
