9b3d0fb5468eabdc2f51aa75aa90a6e20d0757ce
Isaac Sousa <isaaslima@yahoo.com.br>
2021-10-30 23:52:01 -0300

    adjust current and remove console

--- src/game.js
+++ src/game.js
@@ -11,37 +11,42 @@ class Game extends React.Component {
       xIsNext: true
     };
   }
+
   handleClick(i) {
     const history = this.state.history;
-    const current = history[history.length -1];
+    const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if(calculateWinner(squares) || squares[i]){
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{
-        squares: squares,
+        squares: squares
       }]),
       xIsNext: !this.state.xIsNext,
     });
   }
+  
   render() {
     const history = this.state.history;
-    const current = history[history.lenght - 1];
-    console.log(current)
+    const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
-    
+
     let status;
     if (winner) {
       status = `Winner: ${winner}`;
     } else {
       status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
     }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares={current.squares} onClick={(i)=> this.handleClick(i)}/>
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
           <div>{status}</div>
